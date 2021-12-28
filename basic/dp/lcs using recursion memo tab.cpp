b43633e6978c85fbb31304fb614d462421ffb8e3
Pradeep Periyasamy <58174391+PRADEEPERIYASAMY@users.noreply.github.com>
2021-12-28 10:22:15 +0530

    feat : dp 2

--- basic/dp/lcs using recursion memo tab.cpp
+++ basic/dp/lcs using recursion memo tab.cpp
@@ -31,8 +31,8 @@ int memo[1000][1000];
 
 int lcs(string s1,string s2,int n,int m){
     int dp[n+1][m+1];
-    for (int i = 0; i < n; i++) dp[i][0] = 0;
-    for (int i = 0; i < m; i++) dp[0][i] = 0;
+    for (int i = 0; i <= n; i++) dp[i][0] = 0;
+    for (int i = 0; i <= m; i++) dp[0][i] = 0;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             if(s1[i-1] == s2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
