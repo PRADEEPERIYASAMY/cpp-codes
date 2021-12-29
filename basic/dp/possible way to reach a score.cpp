2ac71a74b9a6d31004e5336555af8c7f69ac7e3c
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2021-12-29 10:53:09 +0530

    feat: dp 22

---
+++ basic/dp/possible way to reach a score.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+//dp
+int count(int n){
+    int dp[n+1];
+    memset(dp,0,sizeof(dp));
+    dp[0] = 1;
+    for (int i = 3; i <= n; i++) dp[i]+=dp[i-3];
+    for (int i = 5; i <= n; i++) dp[i]+=dp[i-5];
+    for (int i = 10; i <= n; i++) dp[i]+=dp[i-10];
+    
+    return dp[n];
+}
+ 
+int main() {
+	int n = 20;
+    cout << "Count for " << n << " is " << count(n) << endl;
+    return 0;
+}
+
