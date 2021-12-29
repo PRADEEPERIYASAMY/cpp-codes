4719202e8eac034beb7436d81f41c0a88479d464
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2021-12-29 11:52:56 +0530

    feat: dp 25

--- basic/dp/hops using 123.cpp
+++ basic/dp/hops using 123.cpp
@@ -5,9 +5,9 @@ using namespace std;
 // order dosen't matter else we can use coin count
 // using fib or recursion
 // int countWays(int n){
-//     if(n == 0) return 0;
+//     if(n <= 0) return 0;
 //     if(n == 1) return 1;
-//     return countWays(n-1)+countWays(n-2);
+//     return countWays(n-1)+countWays(n-2)+countWays(n-3);
 // }
 
 // dp
@@ -15,12 +15,16 @@ int countWays(int n){
     int dp[n+1];
     dp[0] = 0;
     dp[1] = 1;
-    for (int i = 2; i <= n; i++) dp[i] = dp[i-1]+dp[i-2];
+    dp[2] = dp[0]+dp[1];
+    for (int i = 3; i <= n; i++){
+        dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+    }
     return dp[n];
 }
  
 int main() {
-    int n = 10;
+    int n = 4;
     cout << countWays(n+1);
     return 0;
 }
+
