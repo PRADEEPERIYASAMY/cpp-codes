2210c4d320576bec3930ee9cda5efea8ce278b21
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2021-12-31 08:11:42 +0530

    feat: tree 3

---
+++ basic/tree/preorder.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+struct Node{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    Node(int k){
+        key = k;
+        left = right = nullptr;
+    }
+};
+
+void preorder(Node *root){
+    if(root != nullptr){
+        cout<<root->key<<" ";
+        inorder(root->left);
+        inorder(root->right);
+    }
+}
+
+int main() {
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->right->left=new Node(40);
+	root->right->right=new Node(50);
+	
+	preorder(root);
+	
+	return 0;
+}
+
