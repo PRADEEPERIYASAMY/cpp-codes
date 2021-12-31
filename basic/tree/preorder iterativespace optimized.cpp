1340843eba96d368ebaaa4d89d742ab45e8d45c4
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2021-12-31 08:57:46 +0530

    feat: tree 12

---
+++ basic/tree/preorder iterativespace optimized.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+struct Node{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    Node(int k){
+        key = k;
+        left = right = nullptr;
+    }
+};
+
+void preorder(Node *root){
+    if(root == nullptr) return;
+    stack<Node *> s;
+    Node *temp = root;
+    while(temp != nullptr ||!s.empty()){
+        while(temp != nullptr){
+            cout<<temp->key<<" ";
+            if(temp->right != nullptr) s.push(temp->right);
+            temp = temp->left;
+        }
+        if(!s.empty()){
+            temp = s.top();
+            s.pop();
+        }
+    }
+}
+
+int main() {
+    Node *root=new Node(20);
+	root->left=new Node(80);
+	root->right=new Node(30);
+	root->right->left=new Node(40);
+	root->right->right=new Node(50);
+	preorder(root);
+	return 0;
+}
+
