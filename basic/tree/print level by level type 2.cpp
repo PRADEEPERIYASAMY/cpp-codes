51f8f1b3069ea4dbb2d263a8df4fa14c9b007cd0
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2021-12-31 09:15:57 +0530

    feat: tree 14

---
+++ basic/tree/print level by level type 2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+struct Node{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    Node(int k){
+        key = k;
+        left = right = nullptr;
+    }
+};
+
+void printLevel(Node *root){
+    if(root == nullptr) return;
+    queue<Node *> q;
+    q.push(root);
+    while(!q.empty()){
+        int n = q.size();
+        for (int i = 0; i < n; i++) {
+            Node *temp = q.front();
+            q.pop();
+            cout<<temp->key<<" ";
+            if(temp->left != nullptr) q.push(temp->left);
+            if(temp->right != nullptr) q.push(temp->right);
+        }
+        cout<<"\n";
+    }
+}
+
+int main() {
+    Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	root->left->right=new Node(50);
+	root->right->left=new Node(60);
+	root->right->right=new Node(70);
+	printLevel(root);
+	return 0;
+}
+
