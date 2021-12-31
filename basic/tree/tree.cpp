bbeea9b47849c1f5648ba44cdce05809ea40bc57
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2021-12-31 08:06:54 +0530

    feat: tree 1

---
+++ basic/tree/tree.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+struct Node{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    Node(int k){
+        key = k;
+        left = right = nullptr;
+    }
+};
+
+int main() {
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->left->left=new Node(40);
+	return 0;
+}
+
