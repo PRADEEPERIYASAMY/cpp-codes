b77079c875c661e2bc40c659712abea3070b0865
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-01-01 18:06:34 +0530

    feat: graph 8

---
+++ basic/graph/dfs connected components.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+void DFSRec(vector<int> adj[],int s,bool visited[]){
+    visited[s] = true;
+    cout<<s<<" ";
+    for (auto &i : adj[s]) {
+        if(!visited[i]) DFSRec(adj,i,visited);
+    }
+}
+
+int DFS(vector<int> adj[],int v){
+    bool visited[v] = {false};
+    int c = 0;
+    for (int i = 0; i < v; i++) {
+        if(!visited[i]){
+            DFSRec(adj,i,visited);
+            c++;
+        }
+    }
+    return c;
+}
+
+void addEdge(vector<int> adj[], int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+int main(){
+    
+    int V=5;
+	vector<int> adj[V];
+	addEdge(adj,0,1); 
+	addEdge(adj,0,2); 
+	addEdge(adj,1,2);
+	addEdge(adj,3,4);
+
+	cout << "Number of connected components: "<< DFS(adj,V); 
+
+	return 0;  
+}
+
