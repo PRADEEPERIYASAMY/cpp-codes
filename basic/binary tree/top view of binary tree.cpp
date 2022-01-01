3b273bc8bcf73a409f8613ea2e56d7ea426edd0c
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-01-01 10:52:42 +0530

    feat: binary tree 12

---
+++ basic/binary tree/top view of binary tree.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+struct Node  { 
+  int key; 
+  struct Node *left; 
+  struct Node *right;
+  Node(int k){
+      key=k;
+      left=right=nullptr;
+  }
+};
+
+void inorder(Node *root){
+    if(root!=NULL){
+        inorder(root->left);
+        cout<<root->key<<" ";
+        inorder(root->right);
+    }
+}
+
+void topView(Node *root){
+    map<int,int> mp;
+    queue<pair<Node*,int>> q;
+    q.push({root,0});
+    while(!q.empty()){
+        auto temp = q.front();
+        q.pop();
+        Node *cur = temp.first;
+        int hd = temp.second;
+        if(mp.find(hd) == mp.end()) mp[hd] = cur->key;
+        if(cur->left) q.push({cur->left,hd-1});
+        if(cur->right) q.push({cur->right,hd+1});
+    }
+    for (auto &x : mp) {
+        cout<<x.second<<" ";
+    }
+}
+
+int main() {
+    Node *root = new Node(10);  
+    root->left = new Node(20);  
+    root->right = new Node(30);  
+    root->left->left = new Node(40);
+    root->left->right = new Node(50);
+    
+    topView(root);
+          
+    return 0; 
+}
+
