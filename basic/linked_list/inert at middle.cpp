5b463a55d029358daa519a20f092d59c6301a77e
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-01-05 08:55:51 +0530

    feat: linked list 21

---
+++ basic/linked_list/inert at middle.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+struct Node{
+    Node* next;
+    int data;
+    Node(int x){
+        data = x;
+        next = nullptr;
+    }
+};
+
+void printlist(Node *head){
+    if(head == nullptr) return;
+    cout<<head->data<<" ";
+    printlist(head->next);
+}
+
+Node* insertAtMid(Node *head,int x){
+    Node* temp = new Node(x);
+    if(head == nullptr) return temp;
+    Node *fast = head->next,*slow = head;
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    temp->next = slow->next;
+    slow->next = temp;
+    return head;
+}
+
+int main() {
+	Node* head = NULL;
+    head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(4);
+    head->next->next->next = new Node(5);
+    cout << "Linked list before insertion: ";
+    printlist(head);
+    int x = 3;
+    head = insertAtMid(head, x);
+    cout << "\nLinked list after insertion: ";
+    printlist(head);
+    return 0;
+}
+
