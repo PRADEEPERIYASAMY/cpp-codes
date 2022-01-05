62ad2c8b0ff40fa132b2f3b92be395eeef457728
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-01-05 20:42:46 +0530

    feat: linked list 46

---
+++ basic/linked_list/swap kth node from begining and end.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+struct Node{
+    Node* next;
+    int data;
+    Node(int x){
+        data = x;
+        next = nullptr;
+    }
+};
+
+void printList(struct Node* node){
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+}
+
+void push(struct Node** head_ref, int new_data){
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+int countNodes(struct Node* s){
+    int count = 0;
+    while (s != NULL) {
+        count++;
+        s = s->next;
+    }
+    return count;
+}
+
+void swapKth(Node **head,int k){
+    int n = countNodes(*head);
+    if(n<k) return;
+    if(2*k-1 == n) return;
+    Node *x = *head;
+    Node *x_prev = nullptr;
+    for (int i = 1; i <k; i++) {
+        x_prev = x;
+        x = x->next;
+    }
+    Node *y = *head;
+    Node *y_prev = nullptr;
+    for (int i = 1; i <n-k+1; i++) {
+        y_prev = y;
+        y = y->next;
+    }
+    if(x_prev) x_prev->next = y;
+    if(y_prev) y_prev->next = x;
+    Node *temp = x->next;
+    x->next = y->next;
+    y->next =temp;
+    if(k == 1) *head = y;
+    if(k == n) *head = x;
+}
+
+int main() {
+    struct Node* head = NULL;
+    for (int i = 8; i >= 1; i--)
+        push(&head, i);
+    cout << "Original Linked List: ";
+    printList(head);
+    for (int k = 1; k < 9; k++) {
+        swapKth(&head, k);
+        cout << "\nModified List for k = " << k << endl;
+        printList(head);
+    }
+    return 0;
+}
+
