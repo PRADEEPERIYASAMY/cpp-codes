be4399152b22be0dbb07d74ea89ec55e40fe95a9
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-01-18 22:09:11 +0530

    feat: easy 9

--- binarySearch/easy/robinhood.cpp
+++ binarySearch/easy/robinhood.cpp
@@ -1,6 +1,24 @@
 using namespace std;
 #include <bits/stdc++.h>
 
+//int solve(int n, int e, int o, int t) {
+//    int count = 0;
+//    double ta = t*1.0;
+//    double na = n*1.0;
+//    double ea = e*1.0;
+//    double oa = o*1.0;
+//    while(na<ta){
+//        if(count%2 == 0){
+//            na+=((na/100)*ea);
+//        }else{
+//            na+=((na/100)*oa);
+//        }
+//        count++;
+//    }
+//
+//    return count;
+//}
+
 int solve(int n,int e,int o,int t){
     double y1 = e/100.0+1;
     double y2 = o/100.0+1;
