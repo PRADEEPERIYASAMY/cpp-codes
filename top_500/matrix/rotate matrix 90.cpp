e93e511a416cf461942ed94e06d6a4867a0cfbaf
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-01-19 20:12:39 +0530

    feat: matrix 4

---
+++ top_500/matrix/rotate matrix 90.cpp
@@ -0,0 +1,30 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define N 4
+
+void rotateMatrix(int mat[][N]){
+    for (int i = 0; i < N; i++) reverse(mat[i],mat[i]+N);
+    for (int i = 0; i < N; i++) for (int j = i; j < N; j++) swap(mat[i][j],mat[j][i]);
+}
+
+void displayMatrix(int mat[N][N]){
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)printf("%2d ", mat[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main(){
+    int mat[N][N] = {
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 },
+        { 13, 14, 15, 16 }
+    };
+
+    rotateMatrix(mat);
+    displayMatrix(mat);
+    return 0;
+}
+
