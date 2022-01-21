7a51b58a7bd25ff9687e325dc1a901b073ff18fa
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-01-21 16:00:38 +0530

    feat: searching and sorting 21

---
+++ top_500/searching_and_sorting/bucket sort.cpp
@@ -0,0 +1,23 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+void bucketSort(float arr[],int n){
+    vector<float> b[n];
+    for (int i = 0; i < n; i++) {
+        int b1 = n*arr[i];
+        b[b1].push_back(arr[i]);
+    }
+    for (int i = 0; i < n; i++) sort(b[i].begin(),b[i].end());
+    int index = 0;
+    for (int i = 0; i < n; i++) for (int j = 0; j < b[i].size(); j++) arr[index++] = b[i][j];
+}
+ 
+int main(){
+    float arr[] = { 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bucketSort(arr, n);
+    cout << "Sorted array is \n";
+    for (int i = 0; i < n; i++)cout << arr[i] << " ";
+    return 0;
+}
+
