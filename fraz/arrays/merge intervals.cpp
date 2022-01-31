3d1f16ac0e6126c5821705fe1a4f15fa72cc47fe
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-01-31 08:38:57 +0530

    feat: arrays 13

---
+++ fraz/arrays/merge intervals.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> res;
+    if(intervals.empty()) return res;
+    sort(intervals.begin(),intervals.end());
+    for (auto &i : intervals) {
+        if(res.empty()||res.back()[1]<i[0]) res.push_back(i);
+        else res.back()[1] = max(res.back()[1],i[1]);
+    }
+    return res;
+}
+
+int main(){
+    //leet code solutions
+    //https://leetcode.com/problems/majority-element/
+    return 0;
+}
+
