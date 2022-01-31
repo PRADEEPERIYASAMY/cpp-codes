e3674017e36232a35ea579584f9bd023abeaf811
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-01-31 14:39:11 +0530

    feat: arrays 27

---
+++ fraz/arrays/find the duplicate number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int findDuplicate(vector<int>& nums) {
+    int slow = nums[0],fast = nums[nums[0]];
+    while(slow != fast) slow = nums[slow],fast = nums[nums[fast]];
+    slow = 0;
+    while(slow != fast) slow = nums[slow],fast = nums[fast];
+    return slow;
+}
+
+int main() {
+	// leetcode
+	// https://leetcode.com/problems/find-the-duplicate-number/
+	return 0;
+}
+
