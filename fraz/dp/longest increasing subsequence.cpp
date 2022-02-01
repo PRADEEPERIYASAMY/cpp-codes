7866fbc5ae9581187da5abffe53f4cd4e3ebcd31
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-02-01 15:01:41 +0530

    feat: dp 16

---
+++ fraz/dp/longest increasing subsequence.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int lengthOfLIS(vector<int>& nums) {
+    if(nums.empty()) return 0;
+    int n = nums.size();
+    vector<int> dp(n,1);
+    for (int i = 1; i < n; i++) for (int j = 0; j < i; j++) if(nums[i]>nums[j] && dp[i]<dp[j]+1) dp[i] = dp[j]+1;
+    return *max_element(dp.begin(),dp.end());
+}
+
+int main() {
+	// leetcode
+	// https://leetcode.com/problems/longest-increasing-subsequence/
+	return 0;
+}
+
