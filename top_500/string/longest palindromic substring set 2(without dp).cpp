105cb8dddf4bb302f1c85b3df448042d6ddb64f6
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-02-21 12:56:45 +0530

    feat: string 9

---
+++ top_500/string/longest palindromic substring set 2(without dp).cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestPalSubstr(string str){
+    int n = str.size();
+    if(n<2) return n;
+    int maxLength = 1,start = 0;
+    int low,high;
+    for (int i = 0; i < n; i++) {
+        low = i-1;
+        high = i+1;
+        while(high<n && str[high] == str[i]) high++;
+        while(low>=0 && str[low] == str[i]) low--;
+        while(low>= 0 && high<n && str[low] == str[high]) low--,high++;
+        int length = high - low - 1;
+        if(maxLength < length){
+            maxLength = length;
+            start = low+1;
+        }
+    }
+    cout<<str.substr(start,maxLength);
+    return maxLength;
+}
+
+int main() {
+	// pepcoding
+	// https://www.geeksforgeeks.org/longest-palindromic-substring-set-2/
+	string str = "forgeeksskeegfor";
+    cout << "\nLength is: " << longestPalSubstr(str)<< endl;
+    return 0;
+}
+
