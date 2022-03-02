7d44e449c8b61c86a50774445904b527ef3429f7
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-03-02 11:14:53 +0530

    feat: greedy 12

---
+++ top_500/greedy/paper cut into minimum number of squares.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minimumSquare(int a, int b){
+    long long result = 0,rem = 0;
+    if(a<b) swap(a,b);
+    while(b>0){
+        result+=a/b;
+        long long rem = a%b;
+        a = b;
+        b = rem;
+    }
+    return result;
+}
+
+int main() {
+	// pepcoding
+	// https://www.geeksforgeeks.org/paper-cut-minimum-number-squares/
+    int n = 13, m = 29;
+    cout << minimumSquare(n, m);
+    return 0;
+}
+
