291c8ca46f6cea91ca50ad34d5084677388f50cf
PRADEEPERIYASAMY <papradeep11cs@gmail.com>
2022-03-10 17:19:20 +0530

    feat: stacksAndQueues 17

---
+++ top_500/stacksAndQueues/stack permutation(check if an array is stack permutation of other).cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkStackPermutation(int ip[],int op[],int n){
+    queue<int> input,output;
+    for (int i = 0; i < n; i++) input.push(ip[i]);
+    for (int i = 0; i < n; i++) output.push(op[i]);
+    stack<int> tempStack;
+    while(!input.empty()){
+        int ele = input.front();input.pop();
+        if(ele == output.front()){
+            output.pop();
+            while(!tempStack.empty()){
+                if(tempStack.top() == output.front()) tempStack.pop(),output.pop();
+                else break;
+            }
+        }else tempStack.push(ele);
+    }
+    return input.empty()&&tempStack.empty();
+}
+
+int main() {
+	// pepcoding
+	// https://www.geeksforgeeks.org/stack-permutations-check-if-an-array-is-stack-permutation-of-other/
+	int input[] = {1, 2, 3};
+    int output[] = {2, 1, 3};
+    int n = 3;
+    if (checkStackPermutation(input, output, n)) cout << "Yes";
+    else cout << "Not Possible";
+    return 0;
+}
